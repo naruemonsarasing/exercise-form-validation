70b795f45684ad81e8003d6c6d152f5e28ce9500
codecamp.official.thailand <codecamp.official.thailand@gmail.com>
2020-06-30 15:58:43 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -6,32 +6,101 @@ class App extends React.Component {
   state = {
     formData: {
       username: {
-        value: ""
+        value: "",
+        validator: {
+          minLength: 6,
+          maxLength: 12,
+          required: true,
+        },
+        error: { status: true, message: "", isTouch: false }
       },
       password: {
-        value: ""
+        value: "",
+        validator: {
+          minLength: 12,
+          maxLength: 24,
+          required: true
+        },
+        error: { status: true, message: "", isTouch: false }
       },
       email: {
-        value: ""
+        value: "",
+        validator: {
+          required: true,
+          email: true
+        },
+        error: { status: true, message: "", isTouch: false }
       },
       phoneNumber: {
-        value: ""
+        value: "",
+        validator: {
+          minLength: 9,
+          maxLength: 10
+        },
+        error: { status: true, message: "", isTouch: false }
       },
       firstName: {
-        value: ""
+        value: "",
+        validator: {
+          minLength: 4,
+          maxLength: 12
+        },
+        error: { status: true, message: "", isTouch: false }
       },
-    }
+    },
+    isFormValid: false,
   };
 
   onChangeInput = (e) => {
     const fieldName = e.target.name;
     const fieldValue = e.target.value;
-
     const updatedFormData = { ...this.state.formData };
 
-    updatedFormData[fieldName].value = fieldValue;
+    let { isValid, errorMessage } = this.checkValue(fieldValue, this.state.formData[fieldName].validator);
 
-    this.setState({ formData: updatedFormData });
+    updatedFormData[fieldName].value = fieldValue;
+    updatedFormData[fieldName].error.status = !isValid;
+    updatedFormData[fieldName].error.message = errorMessage;
+    updatedFormData[fieldName].error.isTouch = true;
+
+    let newIsFormValid = true;
+    for (let fn in updatedFormData) {
+      if (updatedFormData[fn].validator.required === true) {
+        newIsFormValid = !updatedFormData[fn].error.status && newIsFormValid;
+      }
+    }
+
+    this.setState({ formData: updatedFormData, isFormValid: newIsFormValid });
+  };
+
+  checkValue = (value, rules) => {
+    let isValid = true;
+    let errorMessage = "";
+    let trimedValue = value.trim();
+
+    if (rules.maxLength && trimedValue.length > rules.maxLength) {
+      isValid = false;
+      errorMessage = `ช่องนี้ต้องยาวไม่เกิน ${rules.maxLength} ตัว`;
+    }
+
+    if (rules.minLength && trimedValue.length < rules.minLength) {
+      isValid = false;
+      errorMessage = `ช่องนี้ต้องยาวอย่างน้อย ${rules.minLength} ตัว`;
+    }
+
+    const regEx = new RegExp("[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+");
+
+    if (rules.email && !regEx.test(trimedValue)) {
+      isValid = false;
+      errorMessage = `รูปแบบ Email ของคุณคืออะไร กรอกใหม่ซิ`;
+    }
+
+    if (rules.required && trimedValue.length === 0) {
+      isValid = false;
+      errorMessage = `กรอกช่องนี้ด้วยนะ`;
+    }
+
+    return { isValid, errorMessage };
   };
 
   onFinish = (e) => {
@@ -50,6 +119,7 @@ class App extends React.Component {
             value={username.value}
             onChangeInput={this.onChangeInput}
             placeholder="Username"
+            error={username.error}
           />
           <Input
             name="password"
@@ -57,26 +127,30 @@ class App extends React.Component {
             onChangeInput={this.onChangeInput}
             type="password"
             placeholder="Password"
+            error={password.error}
           />
           <Input
             name="email"
             value={email.value}
             onChangeInput={this.onChangeInput}
             placeholder="Email"
+            error={email.error}
           />
           <Input
             name="phoneNumber"
             value={phoneNumber.value}
             onChangeInput={this.onChangeInput}
             placeholder="Phone Number"
+            error={phoneNumber.error}
           />
           <Input
             name="firstName"
             value={firstName.value}
             onChangeInput={this.onChangeInput}
             placeholder="First name"
+            error={firstName.error}
           />
-          <button className="Button">Register</button>
+          <button disabled={!this.state.isFormValid} className="Button">Register</button>
         </form>
       </div>
     );
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
 function Input(props) {
-    const { value, onChangeInput, name, placeholder, type } = props;
+    const { value, onChangeInput, name, placeholder, type, error } = props;
+
+    const getClassName = () => {
+        let className = "Input InputElement";
+        if (error.status && error.isTouch) {
+            className = "Invalid " + className;
+        }
+        return className;
+    };
 
     return (
-        <input
-            className="Input InputElement"
-            value={value}
-            onChange={onChangeInput}
-            type={type ? type : "text"}
-            name={name}
-            placeholder={placeholder} />
+        <>
+            <input
+                className={getClassName()}
+                value={value}
+                onChange={onChangeInput}
+                type={type ? type : "text"}
+                name={name}
+                placeholder={placeholder} />
+            <p className="ErrorMessage">{error.message}</p>
+        </>
     );
 }
 
